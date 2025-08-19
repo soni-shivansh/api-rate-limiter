e089490e86a45bb5bac0aab171b766259500bcc6
Shivansh Soni <shivanshsoni568@gmail.com>
2025-08-19 12:04:27 +0530

    feat: Integrate with API Gateway and Product Service (V2)

--- rate-limiter-service/src/main/java/com/shivansh/ratelimiterservice/controller/RateLimiterController.java
+++ rate-limiter-service/src/main/java/com/shivansh/ratelimiterservice/controller/RateLimiterController.java
@@ -6,25 +6,36 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class RateLimiterController {
 
     private final TokenBucketRateLimiterService rateLimiterService;
+    private final WebClient.Builder webClientBuilder;
 
-    public RateLimiterController(TokenBucketRateLimiterService rateLimiterService) {
+    public RateLimiterController(TokenBucketRateLimiterService rateLimiterService, WebClient.Builder webClientBuilder) {
         this.rateLimiterService = rateLimiterService;
+        this.webClientBuilder = webClientBuilder;
     }
 
-    @GetMapping("/api/v1/limited")
-    public ResponseEntity<String> getLimitedResource(HttpServletRequest request) {
+    @GetMapping("/v1/products")
+    public Mono<ResponseEntity<String>> getLimitedResource(HttpServletRequest request) {
         String clientIp = request.getRemoteAddr();
 
         if (rateLimiterService.isAllowed(clientIp)) {
-            return ResponseEntity.ok("Success! You can access the resource (v1 - Redis).");
+            return webClientBuilder.build()
+                    .get()
+                    .uri("http://localhost:8081/products") // The actual protected service
+                    .retrieve()
+                    .toEntity(String.class)
+                    .onErrorResume(WebClientResponseException.class,
+                            ex -> Mono.just(ResponseEntity.status(ex.getStatusCode()).body(ex.getResponseBodyAsString())));
         } else {
-            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
-                    .body("Too Many Requests! Please try again later (v1 - Redis).");
+            return Mono.just(ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+                    .body("Too Many Requests! Please try again later."));
         }
     }
 
